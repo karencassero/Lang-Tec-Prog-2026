523bbfc336391e8dc9292e91d0b5c8e40f1f1cf7
karencassero <86863332+karencassero@users.noreply.github.com>
2026-08-05 22:26:27 -0300

    Add files via upload

---
+++ ex2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	float a,b,c;
+	float resultado;
+	
+	a = 10;
+	b = 20;
+	c = 30;
+	
+	resultado = (a+b+c)/3;
+	
+	printf("resultado %f", resultado);
+	
+	return 0;
+	
+}
