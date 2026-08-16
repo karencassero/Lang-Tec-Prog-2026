78b9e93fc2de9232094316706cf0c09b6bc911e8
karencassero <86863332+karencassero@users.noreply.github.com>
2026-08-16 15:40:24 -0300

    Exercício Triangulo

---
+++ ExtTriangulo.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+
+    int base, altura;
+    float area;
+
+    base = 10; 
+	  altura = 20;	
+	
+	  area = (base*altura)/2;
+	
+	  printf("A area do Triangulo Retangulo de Base = %d e Altura = %d > %f", base, altura, area); 
+	
+	  return 0; 
+}
