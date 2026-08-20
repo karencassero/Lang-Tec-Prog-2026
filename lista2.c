cce1b69063bbd589b5875f8392695910368a0595
karencassero <86863332+karencassero@users.noreply.github.com>
2026-08-19 21:42:28 -0300

    Add files via upload

---
+++ lista2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+	
+// Exercício 10	
+	int a, b, c, maior_temp, maior;
+	printf ("Insira os valores a serem comparados: ");
+	scanf ("%d %d %d", &a, &b, &c);
+	
+	maior_temp = ((a+b)+abs(a-b))/2;
+	maior = (maior_temp+c+abs(maior_temp-c))/2;
+	
+	printf ("O maior entre [%d][%d][%d] = %d", a, b, c, maior);
+
+	return 0;
+}
