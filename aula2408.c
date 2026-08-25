f3ab97a90cd974e799a637d28e32ccf644470d81
karencassero <86863332+karencassero@users.noreply.github.com>
2026-08-24 21:36:26 -0300

    Add files via upload

---
+++ aula2408.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	
+	int a, b, c;
+	int resultado;
+	
+	a = 10;
+	b = 1;
+	c = 100;
+
+	resultado = a == b;
+	
+	printf ("%d", resultado);
+	
+	return 0;
+}
