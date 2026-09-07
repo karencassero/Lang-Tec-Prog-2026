2f47925972165a0bb08edc4c98efa4f1f51ec459
karencassero <86863332+karencassero@users.noreply.github.com>
2026-09-07 14:18:34 -0300

    Update Lista1.c

--- Lista1.c
+++ Lista1.c
@@ -145,9 +145,9 @@ int main(int argc, char *argv[]) {
 	p1 = pow(x2-x1, 2);
 	p2 = pow(y2-y1, 2);
 
-	dist = sqrt(p1+p2);
+	dis = sqrt(p1+p2);
 	
-	printf ("\nA distância entre P1 e P2 = %f, dis);
+	printf ("\nA distância entre P1 e P2 = %f", dis);
 }
 	
 	return 0;
